889976b18ef7878c0f8fd53c7d235c10143a4767
Jeffrey van der Leije <j.vanderleije@bmade.nl>
2024-01-03 17:00:43 +0100

    Restructure

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import Stats from "./Stats";
 import Textarea from "./Textarea";
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from "../lib/constants";
 
 export default function Container() {
   const [text, setText] = useState("");
   const numberOfWords = text.split(/\s/).filter((word) => word !== "").length;
   const numberOfCharacters = text.length;
-  const INSTAGRAM_MAX_CHARACTERS = 280;
-  const FACEBOOK_MAX_CHARACTERS = 2200;
   const instagramCharactersLeft = INSTAGRAM_MAX_CHARACTERS - numberOfCharacters;
   const facebookCharactersLeft = FACEBOOK_MAX_CHARACTERS - numberOfCharacters;
 
